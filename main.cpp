49e356d78c9ab50438c04639f9cf7fb39b334100
arlesian <arlesian@snu.ac.kr>
2024-06-13 14:50:53 +0000

    assignment

--- main.cpp
+++ main.cpp
@@ -7,12 +7,26 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
+   
+   if (cnt == M){
+      for (int i = 0; i < M; ++i) cout << vec[i];
+      cout << " ";
+   }
+   else if (vec.empty()){
+      for (int i = 1; i <= N; ++i){
+         vec.push_back(i);
+         func(++cnt);
+         vec.pop_back();
+         cnt--;
+      }
+   } else {
+      for (int i = vec[cnt-1]+1; i <= N; ++i){
+         vec.push_back(i);
+         func(++cnt);
+         vec.pop_back();
+         cnt--;
+      }
+   }
 
 }
 
